a247323d1aff5442a0bb5dd3a76ced808cd18a0b
Oreo-1 <121080759+Oreo-1@users.noreply.github.com>
2025-01-05 11:14:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -37,9 +37,9 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '-5.144794',
+	defaultLongitude: '119.417670',
 
 	// Autochange
 	autoChangeTheme: true,
